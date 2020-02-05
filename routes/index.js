3be8078d1b20f70cb111209e49959d999e8ea888
Vivek Honule <vivekhonule@gmail.com>
2020-02-05 19:26:39 +0530

    implemented functionality to retrieve all the registrations

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const { body, validationResult } = require('express-validator/check');
 
 const router = express.Router();
+const Registration = mongoose.model('Registration');
 
 router.get('/', (req, res) => {
   res.render('form', { title: 'Registration form' });
 });
+
+router.get('/registration', (req, res) => {
+  Registration.find()
+    .then((registrations) => {
+      res.render('index', { title: 'Listing registrations', registrations });
+    })
+    .catch(() => { res.send('Sorry! Something went wrong.'); });
+})
 
 router.post('/',
   [
@@ -20,7 +30,10 @@ router.post('/',
     const errors = validationResult(req);
 
     if (errors.isEmpty()) {
-      res.send('Thank you for your registration!');
+      const registration = new Registration(req.body);
+      registration.save()
+        .then(() => { res.send('Thank you for your registration!'); })
+        .catch(() => { res.send('Sorry! Something went wrong.'); })
     } else {
       res.render('form', {
         title: 'Registration form',
