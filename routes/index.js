29bba4e7a1c1ae4cb0fc1de3c825edc7aa135119
Vivek Honule <vivekhonule@gmail.com>
2020-02-13 18:56:14 +0530

    Implemented functionality to authenticate users before seeing list of registrations

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const { body, validationResult } = require('express-validator/check');
+const path = require('path')
+const auth = require('http-auth')
 
 const router = express.Router();
 const Registration = mongoose.model('Registration');
+
+const basic = auth.basic({
+  file: path.join(__dirname, '../users.htpasswd'),
+});
 
 router.get('/', (req, res) => {
   res.render('form', { title: 'Registration form' });
 });
 
-router.get('/registration', (req, res) => {
+router.get('/registration', auth.connect(basic), (req, res) => {
   Registration.find()
     .then((registrations) => {
       res.render('index', { title: 'Listing registrations', registrations });
