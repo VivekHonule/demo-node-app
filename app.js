69abc82c43540b32efbdf23f580b29a98d06e3ca
Vivek Honule <vivekhonule@gmail.com>
2020-02-20 20:08:08 +0530

    refactored app.js

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const path = require('path');
-const routes  = require('./routes/index');
-const login_router = require('./routes/login');
-const bodyParser  = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use('/', routes);
-app.use('/login', login_router)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/login', require('./routes/login'))
+app.use('/', require('./routes/index'));
+
 app.use(express.static('public'));
 
 module.exports = app
