6dab7a20c86f76a507d502a418408d2186f4c2c6
Vivek Honule <vivekhonule@gmail.com>
2020-02-20 20:09:48 +0530

    created another router for login urls

---
+++ routes/login.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const login_router = express.Router();
+
+login_router.get('/login', (req, res) => {
+    res.render('login');
+  })
+
+  login_router.post('/login', (req, res) =>{
+    res.send('Login attempted');
+  })
+
+  module.exports = login_router; 
